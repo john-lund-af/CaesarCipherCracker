12646111ab82615312f60ba2401d5410bb4289e1
John Lundgren <john.lund.af@gmail.com>
2026-02-20 07:17:20 +0100

    added param symbols to decrypt function

--- cipher_utils.py
+++ cipher_utils.py
@@ -1,12 +1,10 @@
 
-SWEDISH_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ".casefold()
-
-
-def decrypt(cc_message: str, key: int) -> str:
+def decrypt(cc_message: str, key: int, symbols: str) -> str:
     """
     Decrypts a Caesar Cipher message
     :param cc_message: A string containing the Caesar cipher message
     :param key: Key that was used to encrypt the message
+    :param symbols: A string containing the symbols to use to decipher the message (e.g. english alphabet)
     :return: Decrypted message
     """
 
@@ -14,16 +12,16 @@ def decrypt(cc_message: str, key: int) -> str:
 
     for ch in cc_message:
         # Just adds the character to the decrypted_chs list if the character is not part of the swedish alphabet
-        if ch.casefold() not in SWEDISH_ALPHABET:
+        if ch.casefold() not in symbols:
             decrypted_chs.append(ch)
             continue
 
-        position: int = SWEDISH_ALPHABET.find(ch.casefold()) - key
+        position: int = symbols.find(ch.casefold()) - key
 
         if position < 0:
-            position = len(SWEDISH_ALPHABET) + position
+            position = len(symbols) + position
 
-        ch = SWEDISH_ALPHABET[position]
+        ch = symbols[position]
         decrypted_chs.append(ch)
 
     return "".join(decrypted_chs)
